6c62e5108bb54a6dcd0a05b1703469d0e8dc3cc6
Koshi.Michisaka <20223519+michisaka@users.noreply.github.com>
2018-10-22 00:16:21 +0900

    Add drawing thread

--- app/curses-view.c
+++ app/curses-view.c
@@ -2,26 +2,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <pthread.h>
 
 #include "cassl1.h"
 #include "application.h"
 #include "curses-view.h"
 #include "version.h"
 
+typedef struct drawing_param {
+  int cell_size;
+  int step;
+  int top;
+  int left;
+  const option *option;
+} drawing_param;
+
+pthread_mutex_t curses_lock = PTHREAD_MUTEX_INITIALIZER;
+
 static state_num_t* allocate_cell_array(size_t cell_size);
 static void cleanup_curses(void);
 static void signal_handler(int sig);
+static void* drawing_thread(void *arg);
 
 int start_curses_view(const option *option)
 {
-  int i;
   int ret;
-  int step;
-  int cell_size;
-  int top;
-  int left;
-  state_num_t *cell_array;
   short key_input;
+
+  pthread_t drawing_thread_id;
+  drawing_param param;
+
+  param.option = option;
 
   slk_init(1);
 
@@ -53,113 +64,66 @@ int start_curses_view(const option *option)
   }
   draw_status_window(option);
 
-  cell_array = NULL;
-  for (cell_size = option->cell_size; cell_size <= option->loop_end; cell_size++) {
-    if (cell_array != NULL) {
-      free(cell_array);
-    }
-    if ((cell_array = allocate_cell_array(cell_size)) == NULL) {
-      cleanup_curses();
-      return ERR_OUT_OF_MEMORY;
-    }
-
-    update_cell_count(cell_size);
-    if ((create_cell_window(cell_size, option->cell_width)) != SUCCESS) {
-      cleanup_curses();
-      free(cell_array);
-      return ERR_OUT_OF_MEMORY;
-    }
-    refresh();
-
-    top = 0;
-    left = 0;
-    ret = SUCCESS;
-    for (step = 0; step <= 3 * cell_size; step++) {
-      update_step_count(step);
-      update_cell_window(step, cell_array, cell_size, option->cell_width);
-
-      if (step - top == LINES - 6) {
-	top++;
-      }
-      draw_cell_window(top, left);
-
-      switch (ret) {
-      case SYNCHRONIZE:
-      	add_sync_count(1);
-	break;
-      case ERR_NOT_SYNCHRONIZE:
-      	add_not_sync_count(1);
-	break;
-      case ERR_UNDEFINED_RULE:
-      	add_undefined_count(1);
-	break;
-      }
-      doupdate();
-
-      for (i = 0; i < option->interval; i++) {
-	key_input = wgetch(stdscr);
-	flushinp();
-	switch (key_input) {
-	case KEY_RESIZE:
-	  left = 0;
-	  top = 0;
-	  if (resize_status_window() == ERR) {
-	    cleanup_curses();
-	    free(cell_array);
-	    return ERR_CURSES_ERROR;
-	  }
-	  draw_status_window(option);
-	  draw_cell_window(top, left);
-	  doupdate();
-	  break;
-	case KEY_LEFT:
-	  if (left >= option->cell_width) {
-	    left -= option->cell_width;
-	  }
-	  draw_cell_window(top, left);
-	  doupdate();
-	  break;
-	case KEY_RIGHT:
-	  if ((int)(cell_size * option->cell_width) -
-	      (left + (COLS - 5)) >= option->cell_width) {
-	    left += option->cell_width;
-	  }
-	  draw_cell_window(top, left);
-	  doupdate();
-	  break;
-	case KEY_UP:
-	  if (top > 0) {
-	    top -= 1;
-	  }
-	  draw_cell_window(top, left);
-	  doupdate();
-	  break;
-	case KEY_DOWN:
-	  if (step - (top + (LINES - 7)) > 0) {
-	    top += 1;
-	  }
-	  draw_cell_window(top, left);
-	  doupdate();
-	  break;
-	case KEY_F(8):
-	  cleanup_curses();
-	  free(cell_array);
-	  return SUCCESS;
-	}
-	usleep(1000);
-      }
-
-      if (ret != SUCCESS) {
-	break;
-      }
-      ret = change_state(cell_array, cell_size + 2);
-    }
+  if (pthread_create(&drawing_thread_id, NULL, drawing_thread, (void*)&param) != 0) {
+    cleanup_curses();
+    return ERR_THREAD_REEOR;
   }
 
+  while (1) {
+    pthread_mutex_lock(&curses_lock);
+    key_input = wgetch(stdscr);
+    pthread_mutex_unlock(&curses_lock);
+    switch (key_input) {
+    case KEY_RESIZE:
+      param.left = 0;
+      param.top = 0;
+      if (resize_status_window() == ERR) {
+	cleanup_curses();
+//        free(cell_array);
+	return ERR_CURSES_ERROR;
+      }
+      draw_status_window(option);
+      draw_cell_window(param.top, param.left);
+      doupdate();
+      break;
+    case KEY_LEFT:
+      if (param.left >= option->cell_width) {
+	param.left -= option->cell_width;
+      }
+      draw_cell_window(param.top, param.left);
+      doupdate();
+      break;
+    case KEY_RIGHT:
+      if ((int)(param.cell_size * option->cell_width) -
+	  (param.left + (COLS - 5)) >= option->cell_width) {
+	param.left += option->cell_width;
+      }
+      draw_cell_window(param.top, param.left);
+      doupdate();
+      break;
+    case KEY_UP:
+      if (param.top > 0) {
+	param.top -= 1;
+      }
+      draw_cell_window(param.top, param.left);
+      doupdate();
+      break;
+    case KEY_DOWN:
+      if (param.step - (param.top + (LINES - 7)) > 0) {
+	param.top += 1;
+      }
+      draw_cell_window(param.top, param.left);
+      doupdate();
+      break;
+    case KEY_F(8): /* EXIT */
+      cleanup_curses();
+//      free(cell_array);
+      return SUCCESS;
+    }
+  }
   cleanup_curses();
-  free(cell_array);
-
-  return SUCCESS;
+//      free(cell_array);
+  return 0;
 }
 
 static state_num_t* allocate_cell_array(size_t cell_size)
@@ -201,4 +165,70 @@ static void signal_handler(int sig)
 {
   cleanup_curses();
   exit(EXIT_FAILURE);
+}
+
+static void* drawing_thread(void *arg)
+{
+  int ret;
+
+  state_num_t *cell_array;
+  drawing_param *param;
+  const option *option;
+  param = (drawing_param*)arg;
+  option = param->option;
+
+
+  cell_array = NULL;
+  for (param->cell_size = option->cell_size; param->cell_size <= option->loop_end; param->cell_size++) {
+    if (cell_array != NULL) {
+      free(cell_array);
+    }
+    if ((cell_array = allocate_cell_array(param->cell_size)) == NULL) {
+      cleanup_curses();
+      pthread_exit((void*)ERR_OUT_OF_MEMORY);
+    }
+
+    update_cell_count(param->cell_size);
+    if ((create_cell_window(param->cell_size, option->cell_width)) != SUCCESS) {
+      cleanup_curses();
+      free(cell_array);
+      pthread_exit((void*)ERR_OUT_OF_MEMORY);
+    }
+    refresh();
+
+    param->top = 0;
+    param->left = 0;
+    ret = SUCCESS;
+
+    for (param->step = 0; param->step <= 3 * param->cell_size; param->step++) {
+      update_step_count(param->step);
+      update_cell_window(param->step, cell_array, param->cell_size, option->cell_width);
+
+      if (param->step - param->top == LINES - 6) {
+	param->top++;
+      }
+      draw_cell_window(param->top, param->left);
+
+      switch (ret) {
+      case SYNCHRONIZE:
+      	add_sync_count(1);
+	break;
+      case ERR_NOT_SYNCHRONIZE:
+      	add_not_sync_count(1);
+	break;
+      case ERR_UNDEFINED_RULE:
+      	add_undefined_count(1);
+	break;
+      }
+      doupdate();
+      usleep(1000 * option->interval);
+
+      if (ret != SUCCESS) {
+	break;
+      }
+      ret = change_state(cell_array, param->cell_size + 2);
+    }
+  }
+  free(cell_array);
+  return (void*)SUCCESS;
 }
